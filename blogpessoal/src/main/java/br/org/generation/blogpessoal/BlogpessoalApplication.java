46f9ade2b633b0b233712a990ad12ba8e8ef771a
Leonardo Maiorano <leonardomaiorano@outlook.com.br>
2022-01-19 12:47:30 -0300

    Alterações código

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/BlogpessoalApplication.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/BlogpessoalApplication.java
@@ -1,4 +1,4 @@
-package br.og.generation.blogpessoal;
+package br.org.generation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
