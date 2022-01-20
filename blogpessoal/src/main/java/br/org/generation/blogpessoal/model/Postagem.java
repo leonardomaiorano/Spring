dc19188390f5534818c5acf9a702ba4ffe83276e
Leonardo Maiorano <leonardomaiorano@outlook.com.br>
2022-01-20 11:00:12 -0300

    Relação entre tabelas

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_postagens") // create table tb_postagens(
@@ -18,7 +21,7 @@ public class Postagem {
 	
 	@Id // primary key (id)
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // id auto increment 
-	private Long id;
+	private long id;
 	
 	@NotBlank(message = "O atributo título é obrigatório!")
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 5 e no máximo 100 caracteres!")
@@ -30,12 +33,16 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -61,6 +68,14 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
