4719e54d82458def3498aba52c8186201c015342
Leonardo Maiorano <leonardomaiorano@outlook.com.br>
2022-02-01 12:53:14 -0300

    Add JUnit Tests

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/UsuarioRepository.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.org.generation.blogpessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,8 @@ import br.org.generation.blogpessoal.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-	//select * from tb_usuarios where usuario = "usuario"
 	public Optional<Usuario> findByUsuario(String usuario);
-
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
 }
--- blogpessoal/src/test/java/br/org/generation/blogpessoal/BlogpessoalApplicationTests.java
+++ blogpessoal/src/test/java/br/org/generation/blogpessoal/BlogpessoalApplicationTests.java
@@ -1,4 +1,4 @@
-package br.og.generation.blogpessoal;
+package br.org.generation.blogpessoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
